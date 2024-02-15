39f4940fd0dd85eb74f8fa5e55a67a13afaa664a
Cnblackwell <156455477+Cnblackwell@users.noreply.github.com>
2024-02-15 14:15:06 -0500

    Delete homework3-task2 directory

--- homework3-task2/test_load.data.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-def test_load_data():
-    path_url = 'https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/ea1d6e57-87af-4e23-b722-6c1f5aa18a8d/resource/815aedb5-f9d7-4dcd-a33a-4aa7ac5aac50/download/Dinesafe.csv'
-    dine_safe_TO = load_data(path_url)
-    assert isinstance(dine_safe_TO, pd.DataFrame)
